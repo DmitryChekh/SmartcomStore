0d08eceab98d5e186d7d475dbb7ff4f58a6eac34
DmitryChekh <dimaragainst@gmail.com>
2020-10-25 17:03:16 +0300

    first commit

---
+++ SmartcomStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SmartcomStore.Core;
+using SmartcomStore.Data.Models.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartcomStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid, UserClaim, UserRole, UserLogin, RoleClaim, UserToken>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>(e =>
+            {
+                e.ToTable("User");
+            });
+
+
+            builder.Entity<Role>(e =>
+            {
+                e.ToTable("Roles");
+
+                e.HasData(new Role
+                {
+                    Id = new Guid("6e0b025c-2dc3-4367-a27c-8daf44c854f1"),
+                    ConcurrencyStamp = "6e0b025c-2dc3-4367-a27c-8daf44c854f1",
+                    Name = Constants.Roles.Customer,
+                    NormalizedName = Constants.Roles.Customer.ToUpper()
+                });
+                e.HasData(new Role
+                {
+                    Id = new Guid("9721e9da-6622-4cc9-8e47-ce78637062b9"),
+                    ConcurrencyStamp = "9721e9da-6622-4cc9-8e47-ce78637062b9",
+                    Name = Constants.Roles.Manager,
+                    NormalizedName = Constants.Roles.Manager.ToUpper()
+                });
+            });
+
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.ToTable("UserRoles");
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
+
+
+            builder.Entity<UserClaim>().ToTable("UserClaims");
+            builder.Entity<UserLogin>().ToTable("UserLogins");
+            builder.Entity<UserToken>().ToTable("UserTokens");
+            builder.Entity<RoleClaim>().ToTable("RoleClaims");
+
+            base.OnModelCreating(builder);
+        }
+
+        //entities
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+
+
+        
+    }
+}
---
+++ SmartcomStore/Data/Models/Identity/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartcomStore.Data.Models.Identity
+{
+    public class User : IdentityUser<Guid>
+    {
+        public User() {
+            Id = Guid.NewGuid();
+            // example: 2110-2020
+            Code = $"{DateTime.Now.Day.ToString() + DateTime.Now.Month.ToString()}-{DateTime.Now.Year.ToString()}";
+        }
+
+        public string Name { get; set; }
+        public string Adress { get; set; }
+
+        public string Code { get; set; }
+
+        [Column(TypeName = "decimal(18, 4)")]
+        public decimal Discount { get; set; }
+
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
